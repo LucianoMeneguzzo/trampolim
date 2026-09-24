4bb0481f04f1b87b9c056bed055cf8fd6c1e51a8
Silvia Abe <silviaabebuzatto@gmail.com>
2026-09-24 14:31:43 -0300

    feat: adiciona tela de perfil do aluno

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@
 import { Routes, Route } from 'react-router';
 
 // COMPONENTES
-import { Home } from './pages/Home/Home'
+import Home from './pages/Home/Home';
+import PerfilPublico from './pages/PerfilAluno/PerfilAluno';
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/perfil" element={<PerfilPublico />} />
     </Routes>
   )
 }
