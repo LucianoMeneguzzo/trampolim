e4031d1fa57dd85e9bb6d0fc67e3ec2fa6a87496
Silvia Abe <silviaabebuzatto@gmail.com>
2026-09-24 14:39:49 -0300

    fix: arrumando as rotas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,16 @@
 import { Routes, Route } from 'react-router';
 
 // COMPONENTES
-import Home from './pages/Home/Home';
-import PerfilPublico from './pages/PerfilAluno/PerfilAluno';
+import Home             from './pages/Home/Home'
+import PerfilAluno      from './pages/PerfilAluno/PerfilAluno'
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/perfil" element={<PerfilPublico />} />
+      <Route path="/student">
+        <Route path="profile" element={<PerfilAluno />} />
+      </Route>
     </Routes>
   )
 }
